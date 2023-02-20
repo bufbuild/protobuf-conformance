c185fed0852c22a9e9e54e978f0b3257e4521ec3
Steve Ayers <sayers@buf.build>
2023-02-19 23:00:56 -0500

    Make

--- report.js
+++ report.js
@@ -93,7 +93,7 @@ Items of note related to the testing setup of the above libraries:
 The google-protobuf test runner was mostly copied directly from the 
 [google-protobuf repository](https://github.com/protocolbuffers/protobuf-javascript/tree/main/experimental/runtime/kernel/conformance). 
 
-A few modifications were made to the file to get it to run in this environment.  Those changes are listed in the
+A few modifications were made to the file to enable it to run in this environment.  Those changes are listed in the
 [file header](impl/google-protobuf/runner.ts).
 
 #### Protobuf-ES
