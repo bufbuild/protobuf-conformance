0647d6fb675f08dfbc51ddb12e65200c1da96bad
Timo Stamm <ts@timostamm.de>
2025-05-01 11:37:55 +0200

    Update generate.sh to generate test protos for Edition 2023, Edition proto2, and Edition proto3
    test_messages_proto2.proto still does not compile.

--- impl/protobuf.js/generate.sh
+++ impl/protobuf.js/generate.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# protobuf.js does not provide a standard protobuf plugin, and cannot generate code for editions
+# protobuf.js does not provide a standard protobuf plugin
 # protobuf.js fails to compile test_messages_proto2.proto, which includes group extensions since v27.0
-node_modules/.bin/pbjs -t static-module -w wrapper.js --es6 -o gen/protos_pb.js ../../proto/conformance/conformance.proto ../../proto/google/protobuf/test_messages_proto3.proto
+
+node_modules/.bin/pbjs -t static-module -w wrapper.js --es6 -o gen/protos_pb.js \
+  ../../proto/conformance/conformance.proto \
+  ../../proto/google/protobuf/test_messages_proto3.proto \
+  ../../proto/google/protobuf/test_messages_edition2023.proto \
+  ../../proto/google/protobuf/test_messages_proto2_editions.proto \
+  ../../proto/google/protobuf/test_messages_proto3_editions.proto
 node_modules/.bin/pbts -o gen/protos_pb.d.ts gen/protos_pb.js
